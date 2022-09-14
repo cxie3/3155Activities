05ae8068bf04b47840548c91aea0a4beb7c33a12
Christina <cxie3@uncc.edu>
2022-09-13 22:37:54 -0400

    PythonBasics 1 – added logic to odd_range function

--- PythonBasics1/pythonBasics1.py
+++ PythonBasics1/pythonBasics1.py
@@ -8,9 +8,11 @@
 # Define a function odd_range(num1, num2) that takes a starting number (num1) and an ending number (num2)
 # and returns all odd numbers as an array between num1 (inclusive) and num2 (exclusive)
 def odd_range(num1, num2):
-  # YOUR CODE HERE
-
-  return
+  odd = []
+  for i in range(num1, num2):
+    if i % 2 != 0:
+      odd.append(i)
+  return odd
 
 # Part B. has_lower_case
 # Define a function has_lower_case(s) that takes a string s
@@ -18,8 +20,6 @@ def odd_range(num1, num2):
 # The function should return True indicating that string s has a lower case char
 # otherwise return False
 def has_lower_case(s):
-  # YOUR CODE HERE
-  
   return
 
 # Part C. fizz_buzz
@@ -33,6 +33,4 @@ def has_lower_case(s):
 # if num is does not meet any of the above criteria or is less than
 # or equal to 0 return the num as a string
 def fizz_buzz(num):
-  # YOUR CODE HERE
-  
   return
