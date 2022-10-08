1c2877d48c1359b670803998059d7c6a9791dab5
Christina <cxie3@uncc.edu>
2022-10-07 23:58:13 -0400

    PythonBasics2 - Update Methods

--- PythonBasics2/pythonBasics2.py
+++ PythonBasics2/pythonBasics2.py
@@ -12,8 +12,22 @@
 
 def count_threes(n):
   # YOUR CODE HERE
-  # Divide n by 3 and round down to get the number of multiples
-  return int(n/3)
+  # Part A (count_threes) now needs to return the multiple of three that occurs the most in a string.
+  # For ex, 0939639 would return 9 since it appeared 3 times while the other multiple of 3 appeared less than that.
+  # You only need to worry about single digit multiples of 3 (3, 6, 9).
+  # You must use a dictionary to accomplish this.
+  # creating empty dictionary
+  dict = {}
+  # iterating through each number from example
+  for i in n:
+    # counter
+    if i in dict:
+      dict[i] += 1
+    else:
+      dict[i] = 1
+  result = max(dict, key=dict.get)
+  # return result
+  return int(result)
 
 
 # Part B. longest_consecutive_repeating_char
@@ -21,32 +35,27 @@ def count_threes(n):
 # a string s and returns the character that has the longest consecutive repeat.
 def longest_consecutive_repeating_char(s):
   # YOUR CODE HERE
-  # Initialize the repeating character count
-  repeatingCharacter = 0
-  # Initialize the character to first the letter
-  # of the string
-  theCharacter = s[0]
-  # Set length to lengthof string s
-  length = len(s);
-  # Loop for the length of the string
-  for i in range(length):
-    # Set the character count to 1
-    currentCharacter = 1
-    # Loop next char over and check if they're
-    # The same if not breaks loop and goes to next char in str
-    for j in range(i + 1, length):
-      if (s[i] != s[j]):
-        break
-        # If the character match add to currentCharacter counter
-      currentCharacter = currentCharacter + 1
-      # If the current count is bigger than the
-      # Previous count update it
-    if currentCharacter > repeatingCharacter:
-      repeatingCharacter = currentCharacter
-      # Set the character to the largest consecutive character
-      theCharacter = s[i]
-  # Return the largest character
-  return theCharacter
+  # variables for the dictionary, count, and character
+  dictionary = {}
+  count = 0
+  char = ''
+
+  # iterate through the letters
+  for i in s:
+    if i == char:
+      #if the character is the same, increase the count
+      count += 1
+      if dictionary.__contains__(i):
+        # if the count is bigger than the index in the dictionary, set it to the count of the char
+        if count > dictionary[i]:
+          dictionary[i] = count
+    else:
+      # update the count
+      count = 1
+      dictionary.update({i: count})
+      char = i
+  # not sure where to go on but i did not know how to remove the dict_keys()
+  return dictionary.keys()
 
 
 # Part C. is_palindrome
