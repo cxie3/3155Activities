613c75281b3caa0cdb894fe1734465e0b9c92e3e
Christina <cxie3@uncc.edu>
2022-10-27 15:15:02 -0400

    flask app #3 - added execution path for POST request for new note

--- flask_app/flask03.py
+++ flask_app/flask03.py
@@ -2,7 +2,7 @@
 
 # imports
 import os  # os is used to get environment variables IP & PORT
-from flask import render_template, Flask  # Flask is the web app that we will customize
+from flask import render_template, Flask, request  # Flask is the web app that we will customize
 
 app = Flask(__name__)  # create an app
 
@@ -12,7 +12,7 @@ app = Flask(__name__)  # create an app
 # get called. What it returns is what is shown as the web page
 @app.route('/index')
 def index():
-    a_user = {'name': 'Mogli', 'email': 'mogli@uncc.edu'}
+    a_user = {'name': 'Christina', 'email': 'mogli@uncc.edu'}
 
     return render_template("index.html", user=a_user)
 
@@ -22,8 +22,9 @@ def get_notes():
              2: {'title': 'Second note', 'text': 'This is my second note', 'date': '10-2-2020'},
              3: {'title': 'Third note', 'text': 'This is my third note', 'date': '10-3-2020'}
              }
+    a_user = {'name': 'Christina', 'email': 'mogli@uncc.edu'}
 
-    return render_template("notes.html", notes=notes)
+    return render_template("notes.html", notes=notes, user=a_user)
 
 @app.route('/notes/<note_id>')
 def get_note(note_id):
@@ -31,13 +32,19 @@ def get_note(note_id):
              2: {'title': 'Second note', 'text': 'This is my second note', 'date': '10-2-2020'},
              3: {'title': 'Third note', 'text': 'This is my third note', 'date': '10-3-2020'}
              }
+    a_user = {'name': 'Christina', 'email': 'mogli@uncc.edu'}
 
-    return render_template("note.html", note=notes[int(note_id)])
+    return render_template("note.html", note=notes[int(note_id)], user=a_user)
 
-@app.route('/notes/new')
+@app.route('/notes/new', methods=['GET', 'POST'])
 def new_note():
-    a_user = {'name': 'Mogli', 'email': 'mogli@uncc.edu'}
-    return render_template('new.html', user=a_user)
+    #create mock user
+    a_user = {'name': 'Christina', 'email': 'mogli@uncc.edu'}
+    print('request method is', request.method)
+    if request.method == 'POST':
+        return '<h1> POST method used for this request </h1>'
+    else:
+        return render_template('new.html', user=a_user)
 
 
 app.run(host=os.getenv('IP', '127.0.0.1'), port=int(os.getenv('PORT', 5000)), debug=True)
